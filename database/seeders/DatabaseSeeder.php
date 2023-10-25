98af1265684d3ed499d8e4bb7dd7a53dedcad885
Subhadip Ghorui <subhadipghorui105@gmail.com>
2023-10-25 21:41:47 +0530

    fresh

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'is_superadmin' => true,
+            'email' => 'superadmin@example.com',
+        ]);
+        \App\Models\User::factory()->create([
+            'is_superadmin' => false,
+            'email' => 'user@example.com',
+        ]);
+    }
+}
